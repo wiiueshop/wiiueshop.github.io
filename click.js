552cdc76ab525feb64109c2d2c7cc97d83e8928c
wiiueshop <165443069+wiiueshop@users.noreply.github.com>
2024-04-02 07:34:47 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -1,18 +1,4 @@
 
-  function hover() {
-    var audio = new Audio('sfx/Wii U - Hover.mp3');
-audio.play();
-  }
-
-
-
-
-
-  function back() {
-    var audio = new Audio('sfx/Wii U - Back.wav');
-audio.play();
-  }
-
 
 
   function click() {
@@ -22,17 +8,3 @@ audio.play();
 
 
   
-  
-
-
-    
-    var ImageId = document.getElementById("image");
-        if (ImageId.addEventListener) {
-            ImageId.addEventListener('contextmenu', function(e) {
-                e.preventDefault();
-            }, false);
-        } else {
-            ImageId.attachEvent('oncontextmenu', function() {
-                window.event.returnValue = false;
-            });
-        }
